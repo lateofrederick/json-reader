70ef3056c7e8fd40bedf11ce48890b21574463f9
lateofrederick <lateofrederick@gmail.com>
2019-08-23 15:00:33 +0000

    initial commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <table class="table">
+        <thead class="thead-dark"> 
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Body</th>
+          </tr>
+        </thead>
+    <tbody>
+    <tr v-for="info in data" :key="info.id">
+      <!--<th scope="row">1</th> -->
+      <td>{{info.id}}</td>
+      <td>{{info.title.charAt(0)}}</td>
+      <td>{{info.body.charAt(0)}}</td>
+    </tr> 
+  </tbody>
+      </table>
+    </main>
+  </div>
+</template>
+
+<script>
+import SystemInformation from "./LandingPage/SystemInformation";
+import axios from "axios";
+import { log } from "util";
+
+export default {
+  name: "landing-page",
+  components: { SystemInformation },
+  data() {
+    return {
+      data: null
+    };
+  },
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link);
+    }
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => (this.data = response.data));
+    console.log(this.data);
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
